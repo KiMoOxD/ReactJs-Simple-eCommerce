c9dd2eebcfbbbbd80e24eea7495d46d860d77b92
xZaaaM <25784985+KiMoOxD@users.noreply.github.com>
2024-08-26 17:28:44 +0300

    auth added

--- src/helper/firebase.js
+++ src/helper/firebase.js
@@ -2,17 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyChtATrO1B_c5x9H07eSnBGVAUvNkdlM4c",
-//   authDomain: "simpleecommerce-ff5c5.firebaseapp.com",
-//   projectId: "simpleecommerce-ff5c5",
-//   storageBucket: "simpleecommerce-ff5c5.appspot.com",
-//   messagingSenderId: "664977230837",
-//   appId: "1:664977230837:web:8a9e8acb2a1288b07d2ebc"
-// };
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
